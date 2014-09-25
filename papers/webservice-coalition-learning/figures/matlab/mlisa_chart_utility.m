14dbd079539180923568ed1ba51ba9e5fe6b2986
Marooned202 <maroonedsc@gmail.com>
2014-09-24 20:54:20 -0400

    changes

---
+++ papers/webservice-coalition-learning/figures/matlab/mlisa_chart_utility.m
@@ -0,0 +1,116 @@
+% Create the data to be plotted
+
+color1 = [0.0,0.0,0.5];
+color2 = [0.7,0.7,0.95];
+fontsize = 12;
+
+[v,T,vT]=xlsread('mlisa_chart_utility.xlsx') 
+gainR020 = v((1:1:20),1);
+gainR010 = v((1:1:20),2);
+gainR007 = v((1:1:20),3);
+gainR005 = v((1:1:20),4);
+
+ratioR020 = v((25:1:44),1);
+ratioR010 = v((25:1:44),2);
+ratioR007 = v((25:1:44),3);
+ratioR005 = v((25:1:44),4);
+
+xvalue = (1:1:20)';
+wsname = vT((4:1:23),1);
+
+% Create the pie chart in position 1 of a 2x2 grid
+hFig = figure(1);
+set(hFig, 'Position', [100 100 800 800])
+%set(0,'DefaultAxesColorOrder',[1 0 0],'DefaultAxesLineStyleOrder','-|+|--|.|:')
+set(0,'DefaultAxesColorOrder',[0 0 0;0.5 0.5 0.5])
+
+
+% Create the bar chart in position 1 of a 3x2 grid
+subplot(2, 2, 1);
+%plot(xvalue, normal1, 's-', xvalue, attack1, '*-', 'linewidth', 2, 'markers',4);
+bar(xvalue(1:1:10), gainR020(1:1:10), 0.4, 'FaceColor',color1);
+hold on
+bar(xvalue(11:1:20), gainR020(11:1:20), 0.4, 'FaceColor',color2);
+xlabel('Trained WS           Rational WS','FontSize',fontsize);
+ylabel('Utility Gain','FontSize',fontsize);
+title ('R=0.2 (a)','FontSize',fontsize);
+%title('TITLE');
+axis([0 21 0 400]);
+set(gca,'XTick',[1 5 10 15 20]);
+hleg1 = legend('Trained', 'Rational');
+set(hleg1,'Orientation','horizontal');
+grid on
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+%set(gca,'XTickLabel',wsname);
+%axes('xlim',[0 19],'xtick',xvalue,'xticklabel',wsname)
+
+%hleg1 = legend('Normal data', 'Attacks');
+%set(hleg1, 'Position', [.248,.842,.1,.2]);
+%set(hleg1,'Interpreter','none');
+%set(hleg1,'Orientation','horizontal');
+%set(hleg1,'FontSize',10);
+%text(19,0,'IR=[0.33,0.26,0.20,0.13,0.06,0.04]', 'Rotation', 90);
+
+
+% Create the bar chart in position 2 of a 3x2 grid
+subplot(2, 2, 2);
+bar(xvalue(1:1:10), gainR010(1:1:10), 0.4, 'FaceColor',color1);
+hold on
+bar(xvalue(11:1:20), gainR010(11:1:20), 0.4, 'FaceColor',color2); 
+xlabel('Trained WS           Rational WS','FontSize',fontsize);
+ylabel('Utility Gain','FontSize',fontsize);
+title ('R=0.1 (b)','FontSize',fontsize);
+axis([0 21 0 400]);
+set(gca,'XTick',[1 5 10 15 20]);
+hleg1 = legend('Trained', 'Rational');
+set(hleg1,'Orientation','horizontal');
+grid on
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+
+% Create the stem chart in position 3 of a 2x2 grid
+subplot(2, 2, 3);
+bar(xvalue(1:1:10), gainR007(1:1:10), 0.4, 'FaceColor',color1);
+hold on
+bar(xvalue(11:1:20), gainR007(11:1:20), 0.4, 'FaceColor',color2);
+xlabel('Trained WS           Rational WS','FontSize',fontsize);
+ylabel('Utility Gain','FontSize',fontsize);
+title ('R=0.07 (c)','FontSize',fontsize);
+axis([0 21 0 400]);
+set(gca,'XTick',[1 5 10 15 20]);
+hleg1 = legend('Trained', 'Rational');
+set(hleg1,'Orientation','horizontal');
+grid on
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+
+% Create the line plot in position 4 of a 2x2 grid
+subplot(2, 2, 4);
+bar(xvalue(1:1:10), gainR005(1:1:10), 0.4, 'FaceColor',color1);
+hold on
+bar(xvalue(11:1:20), gainR005(11:1:20), 0.4, 'FaceColor',color2);
+xlabel('Trained WS           Rational WS','FontSize',fontsize);
+ylabel('Utility Gain','FontSize',fontsize);
+title ('R=0.05 (d)','FontSize',fontsize);
+axis([0 21 0 400]);
+set(gca,'XTick',[1 5 10 15 20]);
+hleg1 = legend('Trained', 'Rational');
+set(hleg1,'Orientation','horizontal');
+grid on
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+
+%%%%%%%%%%%%%Figure 2%%%%%%%%%
+
+hFig = figure(2);
+bar(xvalue(1:1:10), [gainR020(1:1:10), gainR010(1:1:10), gainR007(1:1:10), gainR005(1:1:10)], 1);
+xlabel('Web services');
+ylabel('Utility Gain');
+axis([0 11 0 400]);
+legend('R=0.2', 'R=0.1', 'R=0.07', 'R=0.05');
+
+f2hleg1 = legend('R=0.2', 'R=0.1', 'R=0.07', 'R=0.05');
+set(f2hleg1,'Orientation','horizontal');
+set(f2hleg1, 'Position', [.290,.850,.44,.06]);
+
+grid on
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+
+
---
+++ papers/webservice-coalition-learning/figures/matlab/mlisa_roc.m
@@ -0,0 +1,59 @@
+[v,T,vT]=xlsread('mlisa_chart_roc.xlsx') 
+% 'xlsx' for exell 2007
+%v: Double
+%T and vT : cell
+%use v containing numbers 
+
+%set(0,'DefaultAxesColorOrder',[0 0 0],'DefaultAxesLineStyleOrder','-|:|--|-.')
+%set(0,'DefaultAxesColorOrder',[0 0 0],'DefaultAxesLineStyleOrder','-.r*|:|--|-.')
+
+i=0:49;
+it=v(:,24);iy=v(:,24);iz=v(:,24);itt=v(:,24);
+t=v(:,25);y=v(:,29);z=v(:,34);tt=v(:,36);
+%set(gcf,'Position',[100 100 800 500]);
+set(gcf,'Position',[100 100 550 375]);
+
+%subplot(1,3,1);
+
+h = plot(it,t,'--go',iy,y,'--mo',iz,z,':bs',itt,tt,'-.r*', 'MarkerSize',2);
+%set(h(1),'linewidth',3);
+grid on;
+
+%set(gca,'Xcolor',[0.5 0.5 0.5]);
+%set(gca,'Ycolor',[0.5 0.5 0.5]);
+%set(gca,'XTick',1:1:50);
+%set(gca,'YTick',7000:1500:18000);
+%set(gca,'YTick',0:200:1600);
+set(gca,'tickdir','in')
+%set(gca,'XTickLabel',{'1';' ';' ';' ';' ';' ';' ';' ';' ';' ';
+%    '11';' ';' ';' ';' ';' ';' ';' ';' ';' ';
+%    '21';' ';' ';' ';' ';' ';' ';' ';' ';' ';
+%    '31';' ';' ';' ';' ';' ';' ';' ';' ';' ';
+%    '41';' ';' ';' ';' ';' ';' ';' ';' ';'50';});
+set(gca,'XGrid','off','YGrid','on','ZGrid','off');
+set(gca,'YTickLabel',num2str(get(gca,'YTick').'));
+
+
+%axis([0 50 0 1600]);
+
+% Add legend
+hleg1 = legend(strcat('Case1-1, AUC = ',num2str(abs(trapz(it(1:101),t(1:101))))),strcat('Case 1-2, AUC = ',num2str(abs(trapz(iy(1:101),y(1:101))))),strcat('Case 2-1, AUC = ',num2str(abs(trapz(iz(1:101),z(1:101))))),strcat('Case 2-2, AUC = ',num2str(abs(trapz(itt(1:101),tt(1:101))))));
+%set(hleg1,'Location','NorthWest');
+set(hleg1, 'Position', [.66,.235,.1,.2]);
+set(hleg1,'Interpreter','none');
+%set(hleg1,'color','none');
+set(hleg1,'FontSize',10);
+%legend boxoff;
+
+% Add title and axis labels
+% title('Trigonometric Functions', 'fontsize', 10);
+xlabel({'False Positive Rate'}, 'FontSize',14);
+ylabel('Attack Detection Rate', 'FontSize',14);
+
+%Caxes = copyobj(gca,gcf);
+%set(Caxes, 'color', 'none', 'xcolor', 'k', 'xgrid', 'off', 'ycolor','k', 'ygrid','off');
+
+%subplot(1,3,2);
+%plot(i,t,i,y,i,z,'linewidth', 2);
+%Caxes = copyobj(gca,gcf);
+%set(Caxes, 'color', 'none', 'xcolor', 'k', 'xgrid', 'off', 'ycolor','k', 'ygrid','off');
